e053155f74ef8af338a5badc745f8517044c0be8
Felipe Álvarez <alvarezfmb@gmail.com>
2017-07-04 13:55:22 -0300

    ALU Control y Unidad de Control agregadas

--- alu_control.vhd
+++ alu_control.vhd
@@ -2,27 +2,41 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 use IEEE.numeric_std.all;
 
---library work;
---use work.records_pkg.all;
-
-entity alu_contorl is
+entity alu_control is
 	port(
-		clk          : in  std_logic;
-		f            : in  std_logic_vector(5 downto 0); -- funct
-		alu_op_input : in  alu_op_input;
-		alu_input    : out alu_input
+		--Entradas
+		clk    : in  STD_LOGIC;         -- Reloj
+		f      : in  STD_LOGIC_VECTOR(5 downto 0); -- Campo de la instrucción FUNC
+		alu_op : in  std_logic_vector(1 downto 0); -- Señal de control de la Unidad de Control
+		--Salidas
+		alu_in : out ALU_INPUT          -- Entrada de la ALU
 	);
-end entity;
+end alu_control;
 
 architecture behavioral of alu_control is
 begin
 
-	with f & alu_op_input select alu_input <=
-		x"0130" when "0000000",
-		x"0000" when others;
-	ALU_IN.Op0 <= ALU_OP_IN.Op1 and (FUNCT(0) or FUNCT(3));
-	ALU_IN.Op1 <= (not ALU_OP_IN.Op1) or (not FUNCT(2));
-	ALU_IN.Op2 <= ALU_OP_IN.Op0 or (ALU_OP_IN.Op1 and FUNCT(1));
-	ALU_IN.Op3 <= ALU_OP_IN.Op2;
-
-end ALU_CONTROL_ARC;
+process(alu_op, f, alu_in)
+	begin
+		if (alu_op = "00") then
+			alu_in <= "010";
+		elsif alu_op = "01" then
+			alu_in <= "110";
+		elsif alu_op = "10" then
+			if f = "100000" then
+				alu_in <= "010";
+			elsif f = "100010" then
+				alu_in <= "110";
+			elsif f = "100100" then
+				alu_in <= "000";
+			elsif f = "100101" then
+				alu_in <= "001";
+			elsif f = "101010" then
+				alu_in <= "111";
+			end if;
+		else
+			alu_in <= "100";
+end if;
+end process;
+		
+end behavioral;
